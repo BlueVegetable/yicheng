054f85c828d3d725aa694b0bfeff08f3f700bdd1
blueVegetable <2601042086@qq.com>
2018-11-11 19:29:34 +0800

    修改了文件上传的功能

--- src/main/java/cn/studio/zps/blue/ljy/utils/FileUpload.java
+++ src/main/java/cn/studio/zps/blue/ljy/utils/FileUpload.java
@@ -16,9 +16,9 @@ public class FileUpload {
 
     private final static int LENGTH=10240;
 //    private final static String PATH = "D:/Application/apache-tomcat-main/upload";
-//    private final static String PATH = "C:/rongbin-tomcat/apache-tomcat-main/upload";
+    private final static String PATH = "C:/rongbin-tomcat/apache-tomcat-main/upload";
 //    private final static String PATH = "E:/服务器目录/apache-tomcat-first/upload";
-    private final static String PATH = "D:/tomcat/apache-tomcat-8.0.0/upload";
+//    private final static String PATH = "D:/tomcat/apache-tomcat-8.0.0/upload";
     private final static String BANNER_PATH = "/banner-image/";
     private final static String BANNER_PC_PATH = "/banner-pc-image/";
     private final static String UEDITOR_IMAGE_PATH = "/ueditor/image/";
